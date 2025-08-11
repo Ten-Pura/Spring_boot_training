037ea2f3d0c0236df55ea780d395933dd59ba6a1
てんぷら <88696456+Ten-Pura@users.noreply.github.com>
2025-08-11 08:01:51 +0000

    create form and form-controller

--- webapp/src/main/java/com/example/webapp/controller/HomeController.java
+++ webapp/src/main/java/com/example/webapp/controller/HomeController.java
@@ -6,17 +6,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     
-    @RequestMapping("/{temp}")
-    public String index(@PathVariable String temp, Model model) {
-        String msg = "(Empty)"; 
-        if(!temp.isEmpty()) {
-            msg = temp;
-        }
-        model.addAttribute("msg", msg);
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("msg", "フォームを入力してください。");
         return "index";
     }
+
+    @RequestMapping(value="/", method=RequestMethod.POST) 
+    public String form(
+            @RequestParam(value="check1",  required=false)boolean  check1,
+            @RequestParam(value="radio1",  required=false)String   radio1,
+            @RequestParam(value="select1", required=false)String   select1,
+            @RequestParam(value="select2", required=false)String[] select2, 
+            Model model) {
+        
+        String result = "Your input data";
+        result = result + "¥nchecked:" + check1;
+        result = result + "¥nsex:" + radio1;
+        result = result + "¥nSmartPhone:" + select1;
+        result = result + "¥nOS:" + select2[0];
+        model.addAttribute("msg",result);
+        return "index";
+    }
+
 }
