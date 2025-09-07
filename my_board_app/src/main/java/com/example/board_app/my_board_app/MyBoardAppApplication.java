23958337c56fe6e0ecb754b5ebc6c277aa078f47
てんぷら <88696456+Ten-Pura@users.noreply.github.com>
2025-09-07 07:24:00 +0000

    Create My Board App

---
+++ my_board_app/src/main/java/com/example/board_app/my_board_app/MyBoardAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_app.my_board_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBoardAppApplication.class, args);
+	}
+
+}
---
+++ my_board_app/src/test/java/com/example/board_app/my_board_app/MyBoardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_app.my_board_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
